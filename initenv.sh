53cc0716b15a5282c05224dbc264cc6af978357d
Zoltan Kalmar <kalmiz@gmail.com>
2011-01-21 18:58:53 +0100

    - initenv link for tmux config

--- initenv.sh
+++ initenv.sh
@@ -8,5 +8,6 @@ fi
 ln -s $HOME/homeskel/bash_profile $HOME/.bash_profile
 ln -s $HOME/homeskel/bash_aliases $HOME/.bash_aliases
 ln -s $HOME/homeskel/screenrc $HOME/.screenrc
+ln -s $HOME/homeskel/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/homeskel/vimrc $HOME/.vimrc
 ln -s $HOME/homeskel/vim $HOME/.vim

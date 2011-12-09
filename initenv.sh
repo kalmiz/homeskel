2703f523431b494a7645ff33832be60f4ad71583
Zoltan Kalmar <kalmiz@gmail.com>
2011-12-09 14:57:10 -0500

    Added ctags config

--- initenv.sh
+++ initenv.sh
@@ -11,6 +11,7 @@ ln -s $HOME/homeskel/screenrc $HOME/.screenrc
 ln -s $HOME/homeskel/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/homeskel/vimrc $HOME/.vimrc
 ln -s $HOME/homeskel/vim $HOME/.vim
+ln -s $HOME/homeskel/.ctags $HOME/.ctags
 
 if [[ `uname` == "Darwin" ]]; then
     ln -s $HOME/homeskel/tmux_osx.conf $HOME/.tmux_osx.conf
